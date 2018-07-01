40622c099e240289308b6a89140c915f7b6fbd09
mwidjaj <matthewwidjaja@gmail.com>
2018-07-01 10:09:36 +0700

    finished with #3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import path from 'path';
-<<<<<<< HEAD
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
@@ -15,14 +14,6 @@ mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL);
 
 app.use('/api/auth', auth);
-=======
-
-const app = express();
-
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({ errors: { global: "Invalid credentials" } });
-});
->>>>>>> 9d7cecae42c94cb28ff2f48faf628126abc51d03
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
